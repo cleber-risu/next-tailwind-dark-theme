6242a0fe44b5e0c9a82b329928f8c1707f385810
cleber <cleber.r.sudul@gmail.com>
2022-10-01 15:17:18 -0300

    Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head"
+import Footer from "../sections/Footer"
+import Header from '../sections/Header'
+
+interface LayoutProps {
+  children?: any
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>Next Taillwingd Theme</title>
+        <meta name="description" content="Create dark mode in next and tailwind" />
+      </Head>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow bg-red-400">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default Layout

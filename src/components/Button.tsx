05aeeaa7cf6d86347232d42600d00579f851d1ca
cleber <cleber.r.sudul@gmail.com>
2022-10-01 16:08:20 -0300

    Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+interface ButtonProps {
+  children: any
+  className: string
+  onClick: () => void
+}
+
+const Button = ({ children, className, onClick }: ButtonProps) => {
+  return (
+    <button
+      className={`p-2 rounded-md hover:ring-2 hover:ring-gray-300 ${className}`}
+      onClick={onClick}
+    >{children}</button>
+  )
+}
+
+export default Button
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,15 @@
+import Button from "../components/Button"
+
 const Hero = () => {
   return (
     <section className="mt-16">
       <h1 className="text-7xl font-bold">Hi I&apos;m RiSu</h1>
       <h3 className="text-4xl my-3">I am a Full Stack developer</h3>
       <p className="text-gray-700 mb-8">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus eligendi, ducimus fugit quibusdam facere eius fuga suscipit repudiandae quasi velit necessitatibus maiores est dolores quisquam eveniet accusantium exercitationem sunt quae!</p>
-      <button>Hire Me!</button>
+      <Button
+        className="bg-purple-600 text-white px-6"
+        onClick={() => alert('Ok brou!')}
+      >Hire Me!</Button>
     </section>
   )
 }

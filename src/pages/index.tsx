675dab842fbe83f5995a1c5b051b3dda625e362a
cleber <cleber.r.sudul@gmail.com>
2022-10-01 15:53:38 -0300

    Hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
+import Hero from '../sections/Hero'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Hero />
     </div>
   )
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,11 @@
 const Hero = () => {
   return (
-    <div></div>
+    <section className="mt-16">
+      <h1 className="text-7xl font-bold">Hi I&apos;m RiSu</h1>
+      <h3 className="text-4xl my-3">I am a Full Stack developer</h3>
+      <p className="text-gray-700 mb-8">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus eligendi, ducimus fugit quibusdam facere eius fuga suscipit repudiandae quasi velit necessitatibus maiores est dolores quisquam eveniet accusantium exercitationem sunt quae!</p>
+      <button>Hire Me!</button>
+    </section>
   )
 }
 

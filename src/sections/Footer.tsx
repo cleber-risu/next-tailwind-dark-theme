a2be38ed4a27ea1f2d54cb990f46191440e01414
cleber <cleber.r.sudul@gmail.com>
2022-10-01 16:16:01 -0300

    Footer component

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return(
-    <div>
-      Footer
-    </div>
+    <footer className="py-6 text-center text-gray-500 text-sm">
+      <span className="text-gray-900 font-bold text-lg mr-2">RiSu</span>&copy; {new Date().getFullYear()} All rights Reversed
+    </footer>
   )
 }
 
